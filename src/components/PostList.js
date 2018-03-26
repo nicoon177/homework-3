fa4a5214b100e6063de0f21dbe8d19acc6cbacb1
andrushack_M <nicoon177@gmail.com>
2018-03-26 22:45:33 +0300

    changed postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PostListItem from "./PostListItem";
 
-class PostList extends Component {
-    constructor(props) {
-        super (props);
-    }
 
-    render() {
-        return (
-            <div>
-                <PostListItem />
-                <PostListItem />
-                <PostListItem />
-                <PostListItem />
-                <PostListItem />
-                <PostListItem />
-            </div>
-        );
-    }
-}
+const PostList = (props) => {
+    return (
+        <div>
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+            <PostListItem />
+        </div>
+    );
+};
 
 export default PostList;
